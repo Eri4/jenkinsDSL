305735d90864f08bac263dc5405a6dcc5c5952dc
Eri4 <40032099+Eri4@users.noreply.github.com>
2020-02-06 16:35:51 +0100

    Update nodejsdocker.groovy

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -1,4 +1,4 @@
-    job('NodeJS example') {
+    job('NodeJS docker example') {
       scm {
         git('https://github.com/Eri4/jenkinsDSL.git') { node ->
           node / gitConfigName('DSL User')
